096c2d6cbb961239c03f0d0909d91ad548857fbf
madreal3267 <55194680+madreal3267@users.noreply.github.com>
2022-05-04 11:14:44 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name='testoutput',
+  version='0.0.1',
+  description='oss-dev my calculator 0001',
+  author='madreal3267',
+  author_email='madreal3267@gmail.com',
+  url = "https://github.com/madreal3267/testoutput",
+  install_requires=['pytest'],
+  long_description = 'oss-dev calculator python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+      "Programming Language :: Python :: 3",
+      "Operation System :: OS Independent",
+  ],
+)
